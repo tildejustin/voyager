cbfeeed2e6b5875d15fe3e83d17303e03d181ecf
tildejustin <spacepepper3.14@gmail.com>
2023-12-08 20:19:06 -0500

    only apply on when >= java 11 is present

---
+++ src/main/java/me/modmuss50/voyager/MixinPlugin.java
@@ -0,0 +1,41 @@
+package me.modmuss50.voyager;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.*;
+import org.spongepowered.asm.util.JavaVersion;
+import org.spongepowered.asm.util.asm.ASM;
+
+import java.util.*;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return JavaVersion.current() >= JavaVersion.JAVA_11 && ASM.isAtLeastVersion(7);
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
